d4b98d71036a8f5f7a14b8b33b3ac7e924ed9543
MichalNawrot88 <michal.a.nawrot@gmail.com>
2026-04-07 07:23:24 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import pygame
+
+def main():
+    pygame_version = pygame.version.ver
+    print(F"Starting Asteroids with pygame version: {pygame_version}")
+
+if __name__ == "__main__":
+    main()
