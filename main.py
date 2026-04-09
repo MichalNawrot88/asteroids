ced09dce3a41f855be07b1bcc7555f5b860d5242
MichalNawrot88 <michal.a.nawrot@gmail.com>
2026-04-09 05:58:58 +0200

    modified:   .gitignore 	new file:   logger.py 	modified:   main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pygame
+from constants import *
 
 def main():
     pygame_version = pygame.version.ver
     print(F"Starting Asteroids with pygame version: {pygame_version}")
+    print(F"Screen width: {SCREEN_WIDTH}")
+    print(F"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
