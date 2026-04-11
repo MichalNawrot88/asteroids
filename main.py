a0e86db61625d5e2da580beb37abac8ef0daff69
MichalNawrot88 <michal.a.nawrot@gmail.com>
2026-04-11 19:06:24 +0200

    CH4 L3

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import pygame
+import pygame,sys
 from constants import *
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from logger import log_event
     
 def main():
     pygame_version = pygame.version.ver
@@ -12,7 +15,17 @@ def main():
     game_clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    AsteroidField.containers = (updatable)
+    Asteroid.containers = (asteroids,updatable,drawable)
+    Player.containers = (updatable,drawable)
+    
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    asteroids_field = AsteroidField()
     #game loop
     while True:
         log_state()
@@ -20,7 +33,16 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black") 
-        player.draw(screen)   
+        #player.update(dt)
+        #player.draw(screen)   
+        updatable.update(dt)
+        for a in asteroids:
+            if a.collides_with(player) :
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+        for o in drawable :
+            o.draw(screen)
         pygame.display.flip()
         dt = game_clock.tick(60) / 1000
         
